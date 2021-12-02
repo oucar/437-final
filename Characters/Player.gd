478b745efd32ee0ef98d3609efa48f1215918289
Onur Ucar <onurucar@MacBook-Pro-3.local>
2021-12-01 22:01:58 -0500

    added collision for 2x2 tilesets, fixed a bug about tileset

--- Characters/Player.gd
+++ Characters/Player.gd
@@ -7,6 +7,19 @@ var velocity : Vector2
 export(float) var MOVE_SPEED = 200;
 
 
+##### FUNCTIONS ####
+func _physics_process(delta):
+	var input = get_player_input()
+	
+	velocity = Vector2(
+		input.x * MOVE_SPEED,
+		min(velocity.y + GameSettings.GRAVITY, GameSettings.TERMINAL_VELOCITY)
+	)
+	
+	print(velocity.y)
+	move_and_slide(velocity)
+	
+	
 func get_player_input():
 	var input: Vector2
 	
@@ -16,15 +29,7 @@ func get_player_input():
 	
 	return input
 
-func _physics_process(delta):
-	var input = get_player_input()
-	
-	velocity = Vector2(
-		input.x * MOVE_SPEED,
-		0
-	)
-	
-	move_and_slide(velocity)
+
 	
 	
 
---
+++ Singleton/GameSettings.gd
@@ -0,0 +1,7 @@
+extends Node
+
+# GRAVITY
+export(float) var GRAVITY = 50
+
+# CAP SPEED
+export(float) var TERMINAL_VELOCITY = 300
